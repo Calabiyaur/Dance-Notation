f28210e71c0aba13c7a60c7533e0b949df54f14d
Thiemo Fischer <fischer.thiemo@web.de>
2026-03-26 21:59:25 +0100

    Save and load steps

--- BiStepScene.gd
+++ BiStepScene.gd
@@ -7,3 +7,11 @@ var step: BiStep = BiStep.new()
 func _ready() -> void:
 	$Lead.step = step.lead
 	$Follow.step = step.follow
+
+
+func get_lead():
+	return $Lead
+
+
+func get_follow():
+	return $Follow
--- FigureScene.gd
+++ FigureScene.gd
@@ -4,6 +4,8 @@ extends VBoxContainer
 var dance: Dance
 var figure: Figure
 
+var step_button_group: ButtonGroup
+
 
 func _ready() -> void:
 	%Back.pressed.connect(func():
@@ -11,14 +13,39 @@ func _ready() -> void:
 	)
 	%Title.text = dance.name + "\n" + figure.name
 	%Steps/Add.pressed.connect(add_new_step)
+	
+	step_button_group = ButtonGroup.new()
+	step_button_group.allow_unpress = true
+	
+	for step in figure.steps:
+		add_step(step)
+
+
+func add_step(bistep: BiStep):
+	var step = preload("res://BiStep.tscn").instantiate()
+	step.get_lead().button_group = step_button_group
+	step.get_follow().button_group = step_button_group
+	
+	step.step = bistep
+	
+	%Steps.add_child(step)
+	%Steps.move_child(step, %Steps.get_child_count() - 2)
 
 
 func add_new_step():
 	var step = preload("res://BiStep.tscn").instantiate()
+	step.get_lead().button_group = step_button_group
+	step.get_follow().button_group = step_button_group
+	
 	step.step.lead.right_foot = Foot.new()
 	step.step.follow.left_foot = Foot.new()
+	
 	%Steps.add_child(step)
 	%Steps.move_child(step, %Steps.get_child_count() - 2)
+	step.get_lead().button_pressed = true
+	
+	figure.steps.append(step.step)
+	Data.save()
 
 
 func set_dance(dance: Dance):
