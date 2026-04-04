e441f5b0750f36816054fbeb9784b80271f9325b
Thiemo Fischer <fischer.thiemo@web.de>
2026-04-04 15:42:48 +0200

    export

--- scene/App.gd
+++ scene/App.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 		add_dance_button(dance)
 	
 	%Add.pressed.connect(open_create_dance_dialog)
+	%Export.pressed.connect(open_export_dialog)
 
 
 func add_dance_button(dance: Dance) -> Button:
@@ -45,6 +46,23 @@ func open_create_dance_dialog():
 	dialog.popup_centered(Vector2i(280, 60))
 	
 	line.grab_focus()
+
+
+func open_export_dialog():
+	var dialog = FileDialog.new()
+	dialog.title = "Daten exportieren"
+	dialog.access = FileDialog.ACCESS_FILESYSTEM
+	dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+	
+	dialog.confirmed.connect(func():
+		var path = dialog.current_path
+		if not path.ends_with(".tres"):
+			path += ".tres"
+		ResourceSaver.save(data, path)
+	)
+	
+	add_child(dialog)
+	dialog.popup_centered()
 
 
 func delete_dance(button: Button):
