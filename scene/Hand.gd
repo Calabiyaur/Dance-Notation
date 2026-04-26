e26d90bd21bc910f1b3b78b793cb35dcca11fd76
Thiemo Fischer <fischer.thiemo@web.de>
2026-04-26 22:33:19 +0200

    Hand direction

--- scene/Hand.gd
+++ scene/Hand.gd
@@ -7,10 +7,10 @@ func _draw() -> void:
 
 
 func draw_hand(hand: Hand, flip: bool):
-	var offset: Vector2 = Vector2(0.4 if flip else -0.4, 0) * SCALE
+	var offset: Vector2 = Vector2(0.4 if flip else -0.4, 0)
 	
 	draw_ellipse_arc(
-		center + offset,
+		center + offset * SCALE,
 		SCALE.x,
 		SCALE.y * 0.5,
 		TAU * 0.25,
@@ -22,8 +22,8 @@ func draw_hand(hand: Hand, flip: bool):
 	
 	if hand.target:
 		draw_line(
-			center + Vector2(offset.x, (SCALE.y + LINE_WIDTH) * -0.5),
-			center + Vector2(offset.x, (SCALE.y + LINE_WIDTH) * 0.5),
+			center + Vector2(offset.x * SCALE.x, (SCALE.y + LINE_WIDTH) * -0.5),
+			center + Vector2(offset.x * SCALE.x, (SCALE.y + LINE_WIDTH) * 0.5),
 			COLOR,
 			LINE_WIDTH
 		)
@@ -47,3 +47,22 @@ func draw_hand(hand: Hand, flip: bool):
 			COLOR,
 			LINE_WIDTH
 		)
+	
+	if hand.length > 0:
+		var line_from = Vector2.DOWN
+		if (hand.direction == 0 and not flip) or (hand.direction == TAU * 0.5 and flip):
+			line_from.y += 2 * LINE_WIDTH / SCALE.y
+		if hand.direction <= TAU * 0.5:
+			line_from.y *= -1
+		line_from *= 0.5
+		line_from += offset
+		
+		var line_to = line_from + hand.length * Vector2(
+			cos(hand.direction),
+			-sin(hand.direction)
+		)
+		
+		line_from = center + line_from * SCALE
+		line_to = center + line_to * SCALE
+		
+		draw_line(line_from, line_to, COLOR, LINE_WIDTH)
